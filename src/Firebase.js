ed56cb80f6565caca266796ecaf3e06c6c40c9b1
Matthew Klein <mtt.l.kln@gmail.com>
2022-10-31 16:25:58 -0500

    first

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "Firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAadF0XMRCL5mzMyj05iQSO8yWr0HQfXzg",
+  authDomain: "bagelrun-cf28e.firebaseapp.com",
+  projectId: "bagelrun-cf28e",
+  storageBucket: "bagelrun-cf28e.appspot.com",
+  messagingSenderId: "465224694989",
+  appId: "1:465224694989:web:ec0f1e756ff4bc10253d84",
+};
+
+const app = initializeApp(firebaseConfig);
